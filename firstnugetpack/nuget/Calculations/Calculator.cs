0303393cea8e576cbb78ed6e8cd19b84ea674a82
Stipkovits Bence <bencestipkovits@gmail.com>
2022-03-07 18:51:35 +0100

    firstnugetpack

---
+++ firstnugetpack/nuget/Calculations/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    internal class Calculator
+    {
+        public static double Add(double x,double y) { return x + y; }
+        public static double Subtract(double x,double y) { return x - y; }
+        public static double Multiply(double x,double y) { return x * y; }
+        public static double Divide(int x,int y) { return x / y; }
+
+    }
+}
